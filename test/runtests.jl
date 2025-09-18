2a2b69be8191f47f1d25e96e35bfd1b2ce48cec4
GHTaarn <62629455+GHTaarn@users.noreply.github.com>
2025-09-18 12:06:18 +0200

    Tests for array output

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,4 +9,11 @@ using Test
     glyphy(0xffee:0xffff)
     glyphy([43])
     glyphy([43, 123, 2341, 12])
+
+    @testset "array output" begin
+        @test all(size(glyphy("and"; output=:array)) .>= (200, 5))
+        @test glyphy("peacock"; output=:array) == ["1f99a" '🦚' false "peacock" ":peacock:"]
+        @test glyphy(0x20ac; output=:array) == ["020ac" '€' true "euro sign" "euro"]
+        @test glyphy(0x3a:0x3b; output=:array) == ["0003a" ':' true "colon" ""; "0003b" ';' true "semicolon" ""]
+    end
 end
